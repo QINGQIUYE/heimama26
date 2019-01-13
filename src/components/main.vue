a709db242f57ac303ee4fb72cec46b81c1af7aa2
LIqingxiang2018 <liqingxiang@gmil.com>
2019-01-13 20:04:05 +0800

    登录验证

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
-        main
+        main <el-button type="danger" @click="logoinOut" round>危险按钮</el-button>
     </div>
 </template>
 <script>
 export default {
-    
+    beforeCreate() {
+        let token=window.sessionStorage.getItem("token");
+        if (token) {
+            
+        }else{
+            this.$message.error('兄弟你还没有登录')
+              this.$router.push('login')
+        }
+    },
+    methods: {
+        logoinOut(){
+            window.sessionStorage.removeItem('token')
+            this.$router.push('login')
+        }
+    },
 }
 </script>
 <style lang="sass">
